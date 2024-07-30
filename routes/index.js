e22c2f77391d48626d5b023eab56c0b6cd307406
donnacancode <donnacancode@gmail.com>
2024-07-29 21:57:53 -0400

    debug routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
-const userRoutes = require("./api/userRoutes");
-const thoughtRoutes = require("./api/thoughtRoutes");
+const apiRoutes = require("./api");
 
-router.use("/users", userRoutes);
-router.use("/thoughts", thoughtRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
